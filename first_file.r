a7e40949a1ce77a428d9c84821924a0671411757
Nabeel-Ahmed-Tanoli <nabeelahmed753@yahoo.com>
2022-04-07 11:14:52 +0500

    First File created

---
+++ first_file.r
@@ -0,0 +1,8 @@
+# This is first project R file
+print("Hello from R Studio")
+name= "Nabeel Ahmed"
+version= "RStudio -- In Writing"
+print("The name of the author is ")
+print(name)
+print("The version control is ")
+print(version)
